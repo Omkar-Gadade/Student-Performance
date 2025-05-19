308991ec9a4c596782eefdcf1403cc9962b05b15
Omkar-Gadade <omkarumeshgadade@gmail.com>
2025-05-19 22:48:02 +0530

    Added Logger

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
-import logging
+
 import sys
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -21,9 +22,9 @@ class CustomException(Exception):
     
 
 #if __name__ =="__main__":
-   # try:
-       # a=1/0
-     #except Exception as e:
+    #try:
+       #a=1/0
+    #except Exception as e:
         #logging.info("Divide by Zero")
         #raise CustomException(e, sys)
         
